761ace54527d82ac0ab6a26d3e75539959405c94
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-11-09 15:24:42 -0600

    added cards, fixed variables, added roast type submit

--- main.js
+++ main.js
@@ -1,57 +1,83 @@
 "use strict"
 
+// Creates the initial coffee table by taking in the Coffee Array's objects and organizing them
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    // let html = '<tr class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    // html += '<td>' + coffee.name + '</td>';
+    // html += '<td>' + coffee.roast + '</td>';
+    // html += '</tr>';
+    //
+    // return html;
 
-    return html;
+    let coffeeCards = '<div class="card" style="width: 18rem;">';
+    coffeeCards += '<div class="card-body">'
+    coffeeCards += '<h5 class="card-title"> '+ coffee.name +' </h5>'
+    coffeeCards += '<p class="card-text">placeholder info.</p>'
+    coffeeCards += '</div>'
+    coffeeCards += '</div>'
+
+    return coffeeCards
 }
 
+// Takes in the filtered Array of objects and adds to the established Coffee Table
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
+// Takes in the input from Roast Selection and updates the Coffee Table's Content
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value; //Establishes the criteria to filter the Coffee Table
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
+            filteredCoffees.push(coffee); //takes in the Array's criteria and adds it to a new array
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+let coffees = [
+    {id: 101, name: 'Light City', roast: 'light'},
+    {id: 102, name: 'Half City', roast: 'light'},
+    {id: 103, name: 'Cinnamon', roast: 'light'},
+    {id: 201, name: 'City', roast: 'medium'},
+    {id: 202, name: 'American', roast: 'medium'},
+    {id: 203, name: 'Breakfast', roast: 'medium'},
+    {id: 301, name: 'High', roast: 'dark'},
+    {id: 302, name: 'Continental', roast: 'dark'},
+    {id: 303, name: 'New Orleans', roast: 'dark'},
+    {id: 304, name: 'European', roast: 'dark'},
+    {id: 305, name: 'Espresso', roast: 'dark'},
+    {id: 306, name: 'Viennese', roast: 'dark'},
+    {id: 307, name: 'Italian', roast: 'dark'},
+    {id: 308, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+let tbody = document.querySelector('#coffees'); //Holds the Coffee Table contents in HTML
+let submitButtonLight = document.querySelector('#submit-light');
+let submitButtonMedium = document.querySelector('#submit-medium');
+let submitButtonDark = document.querySelector('#submit-dark');
+let roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+submitButtonLight.addEventListener('click', updateCoffees);
+submitButtonMedium.addEventListener('click', updateCoffees);
+submitButtonDark.addEventListener('click', updateCoffees);
+
+
+/*
+- Create 3 variables to count dark, medium, and light roasts
+- Set each variable equal to a function that runs through the 'Coffees' array and counts the number of
+  instances each category appears
+- Make newly inputted coffee's IDs = to variable counter + 1
+
+
+ */
